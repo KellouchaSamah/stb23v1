77a8598709c0c841aa57f38ecf7387e7fac87baf
KellouchaSamah <samah@samah-Latitude-5400>
2023-05-26 16:40:43 +0200

    RestController---> Controller

--- src/main/java/fr/univrouen/stb23v1/controllers/IndexController.java
+++ src/main/java/fr/univrouen/stb23v1/controllers/IndexController.java
@@ -1,8 +1,8 @@
 package fr.univrouen.stb23v1.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+@Controller
 public class IndexController {
 @GetMapping("/")
 public String index() {
